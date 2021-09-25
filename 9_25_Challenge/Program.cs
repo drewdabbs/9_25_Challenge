842ac53e503e4ed360e4cbb5ab4378af348af985
Drew Dabbs <dabbs.drew@gmail.com>
2021-09-25 14:02:15 -0400

    bronze complete at least

--- 9_25_Challenge/Program.cs
+++ 9_25_Challenge/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _9_25_Challenge
 {
@@ -6,7 +7,67 @@ namespace _9_25_Challenge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Bronze:
+            // Part 1 - Print every letter of the word "Supercalifragilisticexpialidocious"
+            // to the console, one at a time.
+            // Part 2 - Next, do the same, except only print the letter if it's an 'i'.
+            // If it's any other letter, print "Not an i".
+
+            // P1: Suggestion?: use Console.WriteLine to print out: char S = 'S';
+            // noting to not repeat char and that 'S' and 's' are different char
+            // P2: If Else statement? If char = 'i' Console.WriteLine(i);
+            // Else Console.WriteLine("Not an i");
+
+            
+
+            Console.WriteLine("Print Supercalifragilisticexpialidocious");  // String of authors  
+            string word = "supercalifragilisticexpialidocious";
+            char[] charArray = word.ToCharArray();
+
+
+
+
+            foreach(char letter in charArray)
+            {
+                
+                if(letter != 'i')
+                {
+                    Console.WriteLine("Not an i"); 
+                }
+                else
+                {
+                    Console.WriteLine(letter);
+                }
+             
+            }
+            foreach (char letter in charArray)
+            {
+                Console.WriteLine(letter);
+            }
+
+
+
+            Console.ReadKey();
+
+
+
+            // Silver:
+            // After that, print the number of letters in the word
+            // (do this with code, not by counting manually and hard-coding the number).
+
+            // Suggestions?
+
+
+
+            // Gold:
+            // In part 2 of Bronze, also determine if the letter is 'L'.
+            // If it is, print 'L'.
+
+            // Suggestion?: As above If char = 'i'; Console.WriteLine(i);
+            // If char = 'l'; Console.WriteLine(l);
+            // Else Console.WriteLine("Not an 'i' or a 'l'.)
+
+
         }
     }
 }
